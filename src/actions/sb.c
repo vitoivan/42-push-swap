bdaf783941bbb63eef0fc65811e8950a5a4df6b7
Victor Ivan <victor.simoes@brmalls.com.br>
2022-11-08 22:44:48 -0300

    fix: adding "sb" to instructions

--- src/actions/sb.c
+++ src/actions/sb.c
@@ -6,7 +6,7 @@
 /*   By: vivan-de <vivan-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:20:30 by vivan-de          #+#    #+#             */
-/*   Updated: 2022/10/26 23:40:42 by vivan-de         ###   ########.fr       */
+/*   Updated: 2022/11/08 22:44:27 by vivan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	sb(t_ctx *ctx)
 		return ;
 	swap_top(&ctx->b);
 	if (ctx->instructions)
-		ft_lstadd_back(&ctx->instructions, ft_lstnew(ft_strdup("sa")));
+		ft_lstadd_back(&ctx->instructions, ft_lstnew(ft_strdup("sb")));
 	else
-		ctx->instructions = ft_lstnew(ft_strdup("sa"));
+		ctx->instructions = ft_lstnew(ft_strdup("sb"));
 }
